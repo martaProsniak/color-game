142ee8d91e8ee6bb7bf3d7c1aa71252b592cf335
martaprosniak <martaprosniak@gmail.com>
2019-08-15 23:24:00 +0200

    Main game logic applied

--- src/colorGame.js
+++ src/colorGame.js
@@ -7,25 +7,38 @@ const colors = [
     "rgb(255, 0, 255)",
 ]
 let squares = document.querySelectorAll('.square');
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById('color-display');
+let messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
-for (let i = 0; i < colors.length; i++){
+for (let i = 0; i < colors.length; i++) {
     //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
 
     //add click listener to squares
-    squares[i].addEventListener('click', function() {
+    squares[i].addEventListener('click', function () {
         //grab color of clicked square
         let clickedColor = this.style.backgroundColor
         //compare color to the picked color
-        if (clickedColor === pickedColor){
-            alert('Correct!');
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = 'Correct!'
+            changeAllSquaresColorAfterWin(clickedColor);
         } else {
-            alert('Wrong!')
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try again!'
         }
-        console.log(clickedColor, pickedColor)
     });
+}
+
+function changeAllSquaresColorAfterWin(color) {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
